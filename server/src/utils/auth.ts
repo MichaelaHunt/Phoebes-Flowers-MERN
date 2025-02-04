4b9778cca97550f01661692d8d34781b2944ec54
Daniel <Danieltwarg@outlook.com>
2025-02-03 19:22:55 -0500

    added signToken function to auth

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -18,6 +18,22 @@ export const authenticateToken = ({ req }: any) => {
 
     //verify the token
     try {
-        const { data }: any = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const { data }: any = jwt.verify(token, process.env.JWT_SECRET_KEY || '', { maxAge: '2h' });
+        //if the token is valid, attach the data to the request object
+        req.user = data;
+    } catch (error) {
+        //if the token is invalid, throw an error
+        console.log('invalid token');
     }
+    //return the request object
+    return req;
 }
+
+export const signToken = (username: string, email: string, _id: unknown) => {
+    //create the payload with user data
+    const payload = { username, email, _id };
+    const secretKey: any = process.env.JWT_SECRET_KEY; //get the secret key from the environment variables
+
+    //sign the token with the payload and secret key, set the expiration to 2 hours
+    return jwt.sign({ data: payload }, secretKey, { expiresIn: '2h' });
+};
