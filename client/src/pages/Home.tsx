a7531d63ceeb4e23f55d1041d677f2b35cf90fc6
Michaela Hunt <micapplehunt@gmail.com>
2025-02-04 14:19:32 -0500

    quick remove unneeded divs

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -28,32 +28,28 @@ function Home() {
             </div>
             {/* Best Seller's Section */}
             <div className="homeSection bestSellerSection">
-                <div>
-                    <h1 className="sectionTitle">Best Sellers</h1>
-                    <div className="itemContainer">
-                        {/* Update later to: Grab 3 random items, then input them into here. */}
-                        <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
-                        <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
-                        <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
-                    </div>
-                    <button className="sectionButton bestSellerButton">Shop Best Sellers</button>
+                <h1 className="sectionTitle">Best Sellers</h1>
+                <div className="itemContainer">
+                    {/* Update later to: Grab 3 random items, then input them into here. */}
+                    <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
+                    <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
+                    <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
                 </div>
+                <button className="sectionButton bestSellerButton">Shop Best Sellers</button>
             </div>
             {/* Gifts to Accompany Section */}
             <div className="homeSection giftSection">
-                <div>
-                    <h1 className="sectionTitle">Gifts to Accompany</h1>
-                    <div className="itemContainer">
-                        {/* Update later to: Grab items of "gift" tag, then input the first 6 into here. */}
-                        <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
-                        <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
-                        <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
-                        <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
-                        <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
-                        <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
-                    </div>
-                    <button className="sectionButton giftButton">Shop Extras</button>
+                <h1 className="sectionTitle">Gifts to Accompany</h1>
+                <div className="itemContainer">
+                    {/* Update later to: Grab items of "gift" tag, then input the first 6 into here. */}
+                    <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
+                    <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
+                    <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
+                    <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
+                    <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
+                    <Item imagePath={image} name="Pale Beach" price={2.50}></Item>
                 </div>
+                <button className="sectionButton giftButton">Shop Extras</button>
             </div>
         </>
     );
