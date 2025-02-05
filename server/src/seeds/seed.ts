2334a01ece7a090c2875722bdc44712da22ec897
Daniel <Danieltwarg@outlook.com>
2025-02-04 21:06:42 -0500

    changed paths in item.json, added imports into client/home.tsx

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,21 +1,21 @@
-// import db from '../config/connection.js';
-// import { Item } from '../models/index.js';
-// import cleanDB from './cleanDB.js';
+import db from '../config/connections.js';
+import { Item } from '../models/index.js';
+import cleanDB from './cleanDB.js';
 
-// import itemData from './itemData.json' assert { type: 'json'};
+import itemData from './itemData.json' assert { type: 'json'};
 
-// const seedDatabase = async (): Promise<void> => {
-//   try {
-//     await db();
-//     await cleanDB();
+const seedDatabase = async (): Promise<void> => {
+  try {
+    await db();
+    await cleanDB();
 
-//     await Item.insertMany(itemData);
-//     console.log('Seeding completed successfully!');
-//     process.exit(0);
-//   } catch (error) {
-//     console.error('Error seeding database:', error);
-//     process.exit(1);
-//   }
-// }
+    await Item.insertMany(itemData);
+    console.log('Seeding completed successfully!');
+    process.exit(0);
+  } catch (error) {
+    console.error('Error seeding database:', error);
+    process.exit(1);
+  }
+}
 
-// seedDatabase();
+seedDatabase();
