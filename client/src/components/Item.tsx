3b684f1d9fab4d4619c1d0411d618df65de36931
Michaela Hunt <micapplehunt@gmail.com>
2025-02-06 21:35:25 -0500

    Fix for dist being build with .json file too; added a few lines to item.tsx

--- client/src/components/Item.tsx
+++ client/src/components/Item.tsx
@@ -1,3 +1,5 @@
+import auth from "../utils/auth";
+
 interface Iitem {
     imagePath: string;
     name: string;
@@ -5,10 +7,22 @@ interface Iitem {
 }
 
 function Item(props: Iitem) {
+    function itemClicked() {
+        if (auth.getToken() != '') {
+            console.log("User labeled as Logged In");
+            //dialog or something?
+        }
+        else {
+            console.log("User labeled as Logged Out!")
+            //alert()
+        }
+        //if they are signed in, then ask how many they'd like
+        //if they are not signed in, pop up a dialog that asks them to please log in. 
+    }
     return (
         <>
             <div className="item column">
-                <img src={props.imagePath}></img>
+                <img src={props.imagePath} onClick={itemClicked}></img>
                 <h3>{props.name}</h3>
                 <p>${props.price}</p>
             </div>
