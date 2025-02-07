303845e33b8ed56dc964e45cccff26a6e5865d73
Daniel <Danieltwarg@outlook.com>
2025-02-06 21:11:59 -0500

    setup imports and function for query in Tag.tsx

--- client/src/pages/Tag.tsx
+++ client/src/pages/Tag.tsx
@@ -2,9 +2,16 @@ import Item from "../components/Item";
 import Nav from "../components/Nav";
 import Title from "../components/Title";
 import image from '../assets/images/bouquet1.png'
+import { QUERY_BY_TAG } from "../utils/queries";
+import { useQuery } from '@apollo/client';
+import { useLocation } from "react-router-dom";
 
+function useQueryByTag() {}
 
+// This page is for displaying all items with a specific tag
 function Tag() {
+    //fetch all items with a specific tag
+
     return (
         <>
             <div className="site">
